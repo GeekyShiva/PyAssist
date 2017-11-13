4676fd392a49a0ca0f2264e4ae3febacf255f943
Shivank Shekhar <shivank1995@gmail.com>
2017-11-14 01:22:57 +0530

    Update Advanced-Wikipedia.py

--- Python_Assistant/PyAssist-BasicFiles/Advanced-Wikipedia.py
+++ Python_Assistant/PyAssist-BasicFiles/Advanced-Wikipedia.py
@@ -3,4 +3,4 @@ import wikipedia
 while True:
         input = raw_input("Q: ")
         wikipedia.set_lang("es")
-        print wikipedia.summary(input, sentences=2)
+        print wikipedia.summary(input, sentences=2) #Restricts to the first two lines only as returned by wiki api
