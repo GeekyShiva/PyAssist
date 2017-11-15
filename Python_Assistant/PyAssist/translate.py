4cb77fab068408b76d5f5e43b05d8cdd8dd126d2
Shivank Shekhar <shivank1995@gmail.com>
2017-11-16 02:38:53 +0530

    Update translate.py

--- Python_Assistant/PyAssist/translate.py
+++ Python_Assistant/PyAssist/translate.py
@@ -2,12 +2,12 @@ def translate(input):
     import goslate
     if input.startswith("translate "):
         gs = goslate.Goslate()
-        trans = input[10:-6]
+        trans = input[10:-7]
         lang = input[-2:]
         print 'Translated: '+trans+' into '+lang
         print gs.translate(trans , lang)
     if 'languages' in input:
         gs = goslate.Goslate()
-        print "I know 90 languages:  use the languages below and there key to translate: "
+        print "I know a lot of languages, almost 90:  use the languages below and there key to translate: "
         espeak.synth("I know 90 languages")
         print gs.get_languages()
