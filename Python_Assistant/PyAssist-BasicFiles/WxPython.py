704d54a55a7a68125a6b0c4a2f363b05cc103acf
Shivank Shekhar <shivank1995@gmail.com>
2017-11-20 00:16:39 +0530

    Update WxPython.py

--- Python_Assistant/PyAssist-BasicFiles/WxPython.py
+++ Python_Assistant/PyAssist-BasicFiles/WxPython.py
@@ -5,11 +5,11 @@ class MyFrame(wx.Frame):
             pos=wx.DefaultPosition, size=wx.Size(450, 100),
             style=wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION |
              wx.CLOSE_BOX | wx.CLIP_CHILDREN,
-            title="PyDa")
+            title="PyAssist")
         panel = wx.Panel(self)
         my_sizer = wx.BoxSizer(wx.VERTICAL)
         lbl = wx.StaticText(panel,
-        label="Hello I am PyAssist the Python Digital Assistant. How can I help you?")
+        label="Hello I am PyAssist the Python Digital Assistant. How may I help you?")
         my_sizer.Add(lbl, 0, wx.ALL, 5)
         self.txt = wx.TextCtrl(panel, style=wx.TE_PROCESS_ENTER,size=(400,30))
         self.txt.SetFocus()
